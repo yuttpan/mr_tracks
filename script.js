b005b2bb669ed1bccac9a0aacfe30e81de9a2f4a
ยุทธพงษ์ อินทรวรรณ < >
2016-12-23 15:50:29 +0700

    d

--- script.js
+++ script.js
@@ -1,50 +1,21 @@
 // create the module and name it scotchApp
-var scotchApp = angular.module('ksnApp', ['ngRoute', 'ngStorage']);
+var App = angular.module('ksnApp', ['ngRoute', 'ngStorage']);
+
+// configure our routes
+App.config(function ($routeProvider) {
+	$routeProvider
+
+		// route for the home page
+		.when('/', {
+			templateUrl: 'pages/main.html',
+			//	controller: 'mainController'
+		})
 
 
-scotchApp.run(function ($rootScope, $location, $state) {
-	$rootScope.$on('$stateChangeStart',
-		function (event, toState, toParams, fromState, fromParams) {
-			console.log('Changed state to: ' + toState);
+		.when('/login', {
+			templateUrl: 'pages/login.html',
+			//	controller: 'loginController'
 		});
 
-	if ($localStorage.status === null) {
-		$state.transitionTo('login');
-	}
-	// configure our routes
-	scotchApp.config(function ($routeProvider) {
-		$routeProvider
+});
 
-			// route for the home page
-			.when('/', {
-				templateUrl: 'pages/main.html',
-				controller: 'mainController'
-			})
-
-			// route for the about page
-			.when('/about', {
-				templateUrl: 'pages/about.html',
-				controller: 'aboutController'
-			})
-
-			// route for the contact page
-			.when('/contact', {
-				templateUrl: 'pages/contact.html',
-				controller: 'contactController'
-			})
-			.when('/login', {
-				templateUrl: 'pages/login.html',
-				controller: 'loginController'
-			});
-
-	});
-
-	// create the controller and inject Angular's $scope
-
-	scotchApp.controller('aboutController', function ($scope) {
-		$scope.message = 'Look! I am an about page.';
-	});
-
-	scotchApp.controller('contactController', function ($scope) {
-		$scope.message = 'Contact us! JK. This is just a demo.';
-	});
--- script/main.js
+++ script/main.js
@@ -1,29 +1,17 @@
-scotchApp.controller('mainController', function ($scope, $http, $location, $localStorage) {
+App.controller('mainController', function ($scope, $http, $location, $localStorage) {
 	// create a message to display in our view
-	console.log($localStorage.stutus)
-	if ($localStorage.status === null) {
 
-			alert("ท่านยังไม่เข้าระบบ กรุณา Login");
-			$scope.st = false;
-			$location.path('/login');
-
-		} else {
-			$scope.user = $localStorage.user;
-			$scope.st = $localStorage.status;
-		}
-
-	console.log($scope.st);
 
 	$scope.form = {};
 
 
-	$scope.logout = function () {
-		$localStorage.status = null;
-		$location.path('/login');
+	/*	$scope.logout = function () {
+			$localStorage.status = null;
+			$location.path('/login');
 
-	}
+		}
 
-
+	*/
 
 
 });
